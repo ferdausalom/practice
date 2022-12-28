6d8ce23b020911826f0e8a6cb7be2efe26b54ae2
Ferdaus <faferdaus@gmail.com>
2022-12-28 23:19:46 +0600

    Update.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,139 @@
 <template>
-  <div class="max-w-[100rem] mx-auto">
-    <router-view />
+  <div class="min-h-screen bg-gray-100">
+    <div class="container mx-auto py-6">
+      <h1 class="font-bold text-4xl text-center text-green-600 mb-8 italic">
+        Al Quran
+      </h1>
+      <div class="bg-white p-4 shadow rounded">
+        <div class="flex -mx-4 items-center mb-6">
+          <div class="flex-1 px-4">
+            <select
+              @change="surahNumber"
+              name="quran-input"
+              id=""
+              class="quran-input"
+            >
+              <option disabled selected value="">Select Sura</option>
+              <option
+                v-for="surah in alquran"
+                :key="surah.id"
+                :value="surah.number"
+              >
+                {{ surah.name }} &nbsp; {{ surah.englishName }}
+              </option>
+            </select>
+          </div>
+          <div class="flex-1 px-4 text-center">
+            <h3 class="font-bold mb-1 text-2xl text-[#35a79c]">
+              {{ surahName.englishName }}
+            </h3>
+            <p>{{ surahName.englishNameTranslation }}</p>
+          </div>
+          <div class="flex-1 px-4">
+            <select @change="ayahNumber" class="quran-input">
+              <option disabled selected value="">Select Ayth</option>
+
+              <option
+                v-for="(ayah, index) in ayahs"
+                :key="index"
+                :value="ayah.numberInSurah"
+              >
+                {{ ayah.numberInSurah }}
+              </option>
+            </select>
+            <button
+              @click="allAyth"
+              class="bg-transparent w-full mt-1 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white px-4 border border-blue-500 hover:border-transparent rounded"
+            >
+              All Ayth
+            </button>
+          </div>
+        </div>
+        <div class="text-4xl mt-8 text-slate-700">
+          <p
+            v-for="(ayah, index) in ayahs"
+            :key="index"
+            class="mb-8 text-right leading-[3rem]"
+          >
+            <span
+              class="text-xs max-w-min px-[.45rem] py-[.1rem] flex items-center justify-center border-green-200 border-2 rounded-full ml-3 mr-6"
+              >{{ ayah.numberInSurah }}
+            </span>
+            {{ ayah.text }}
+
+            <audio id="audioPlayer" controls>
+              <source :src="ayah.audio" type="audio/mpeg" />
+              Your browser does not support the audio element.
+            </audio>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+import axios from "axios";
+import { ref, onMounted, onBeforeUnmount } from "vue";
+
+let alquran = ref([]);
+let ayahs = ref([]);
+let surahName = ref({});
+let items = ref([]);
+
+function allAyth() {
+  ayahs.value = surahName.value.ayahs;
+  const audioPlayer = document.getElementById("audioPlayer");
+  audioPlayer.load();
+}
+
+function surahNumber(e) {
+  getSurah(e.currentTarget.value);
+}
+
+function ayahNumber(e) {
+  items = ayahs.value.filter((item) => {
+    if (e.currentTarget.value == item.numberInSurah) {
+      return item;
+    }
+  });
+
+  ayahs.value = items;
+  const audioPlayer = document.getElementById("audioPlayer");
+  audioPlayer.load();
+}
+
+function getSurah(surahNum) {
+  axios
+    .get("http://api.alquran.cloud/v1/surah/" + surahNum + "/ar.alafasy")
+    .then(function (response) {
+      // handle success
+      ayahs.value = response.data.data.ayahs;
+      surahName.value = response.data.data;
+    });
+}
+
+onMounted(() => {
+  axios.get("http://api.alquran.cloud/v1/surah").then(function (response) {
+    alquran.value = response.data.data;
+  });
+
+  getSurah(1);
+});
+
+onBeforeUnmount(() => {
+  const audioPlayer = document.getElementById("audioPlayer");
+  audioPlayer.load();
+});
+</script>
+
+<style scoped>
+.quran-input {
+  width: 100%;
+  border-radius: 0.25rem;
+  border-width: 1px;
+  --tw-border-opacity: 1;
+  border-color: rgb(229 231 235 / var(--tw-border-opacity));
+  padding: 0.5rem 1rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,38 +6,18 @@
   </h1>
   <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 pt-8">
     <div
-      v-for="(soccer, index) in soccers"
+      v-for="(quran, index) in alquran"
       :key="index"
       class="bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
     >
-      <a href="#">
-        <img
-          class="p-8 w-full rounded-t-lg"
-          :src="soccer.thumbnail"
-          alt="product image"
-        />
-      </a>
       <div class="px-5 pb-5">
-        <a :href="soccer.url">
+        <a>
           <h5
             class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
           >
-            {{ soccer.title }}
+            {{ quran.name }}
           </h5>
         </a>
-        <div class="mt-2.5 mb-5">
-          <p><b>Date:</b> 2022</p>
-          <p><b>Team:</b> {{ soccer.side1.name }}</p>
-          VS
-          <p><b>Team:</b> {{ soccer.side2.name }}</p>
-        </div>
-        <div class="flex items-center justify-between">
-          <a
-            :href="soccer.url"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >Check details</a
-          >
-        </div>
       </div>
     </div>
   </div>
@@ -46,7 +26,7 @@
 <script setup>
 import { ref, onMounted } from "vue";
 
-let soccers = ref([]);
+let alquran = ref([]);
 
 onMounted(() => {
   const options = {
@@ -57,11 +37,11 @@ onMounted(() => {
     },
   };
 
-  fetch("https://free-football-soccer-videos1.p.rapidapi.com/v1/", options)
+  fetch("http://api.alquran.cloud/v1/surah")
     .then((response) => response.json())
     .then((response) => {
-      console.log(soccers);
-      soccers.value = response;
+      console.log(response.data);
+      alquran.value = response.data;
     })
     .catch((err) => console.error(err));
 });
